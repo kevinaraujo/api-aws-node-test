c3b0651617b6811ded2d5b3335ef6d6801481712
Kevin Araújo <>
2022-08-22 16:57:43 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const { response } = require('express')
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    return response.json({ message: 'Server is up' })
+})
+
+app.listen(3333)
