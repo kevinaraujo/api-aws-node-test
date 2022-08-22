79631a1ccf4a281474dd3f58c173ca877e559863
Kevin Araújo <>
2022-08-22 18:14:22 -0300

    Added new route.

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ app.get('/', (req, res) => {
     return res.json({ message: 'Server is up' })
 })
 
+app.get('/update', (req, res) => {
+    return res.json({ message: 'Already updated' })
+})
 app.listen(3333)
